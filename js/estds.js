03caf4e7933c64536436148f693be61c7f088644
estds <56233807+estds@users.noreply.github.com>
2019-10-08 17:25:03 +0200

    Update estds.js

--- js/estds.js
+++ js/estds.js
@@ -3,3 +3,12 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+// facilitate share buttons
+
+$(function () {
+    $(".share-button").each(function () {
+        $(this).attr("href", $(this).attr("href") + "&url=" + document.URL + "&title=" + document.title);
+    });
+});
