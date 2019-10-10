8745b444d39cb0e533c7844b611b5de619ffcb02
estds <56233807+estds@users.noreply.github.com>
2019-10-10 11:04:05 +0300

    Update estds.js

--- js/estds.js
+++ js/estds.js
@@ -1,22 +1,3 @@
-// This is the "Offline page" service worker
-
-// Check compatibility for the browser we're running this in
-if ("serviceWorker" in navigator) {
-  if (navigator.serviceWorker.controller) {
-    console.log("[PWA Builder] active service worker found, no need to register");
-  } else {
-    // Register the service worker
-    navigator.serviceWorker
-      .register("/pwabuilder-sw.js", {
-        scope: "./"
-      })
-      .then(function (reg) {
-        console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
-      });
-  }
-}
-
-
 // Activate Bootstrap tooltips
 
 $(function () {
