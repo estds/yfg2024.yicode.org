fc33fee534b61222248097d998e1cf779f805a8a
estds <56233807+estds@users.noreply.github.com>
2019-10-10 23:37:39 +0100

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -2,7 +2,6 @@ let CACHE = 'cache';
 
 self.addEventListener('install', function(evt) {
     console.log('The service worker is being installed.');
-    evt.waitUntil(precache());
 });
 
 addEventListener('fetch', function(event) {
