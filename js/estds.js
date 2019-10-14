ed013a68478a62817106c3a717bf11c553a4810d
estds <56233807+estds@users.noreply.github.com>
2019-10-14 06:09:45 -0700

    Update estds.js

--- js/estds.js
+++ js/estds.js
@@ -4,19 +4,12 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
-
 // facilitate share buttons
 
 $(function () {
     $(".share-button").each(function () {
         $(this).attr("data-url", $(this).attr("data-url") + "&url=" + document.URL + "&title=" + document.title);
     });
-});
-
-$('.share-button').on('click', function(sharevent) {
-  var surl = $(this).data('url');
-  sharevent.preventDefault();
-  window.open(surl, '', "width=540,height=320");
 });
 
 // encode current url to qr-code
@@ -30,4 +23,37 @@ var qrcode = new QRCode({
   ecl: "M"
 });
 var svg = qrcode.svg();
-document.getElementById("url-qr-wrap").innerHTML = svg;
+$("#url-qr-wrap").append(svg);
+
+
+// targeting popover contents
+$(function toggle_share_bspopup() {
+  $('.share-toggles').popover({
+    //container: 'body',
+    html: true,
+    trigger: 'focus',
+    placement: 'bottom',
+    sanitize: false,
+    content: function() {
+      var pop_target = $(this).attr("data-popover-content");
+      return $(pop_target).children(".popover-content").clone();
+    }
+  });
+})
+
+// using Web Share API if it is available
+const shareButton = document.querySelector('#share-toggle-sns');
+
+shareButton.addEventListener('click', event => {
+  if (navigator.share) { 
+   navigator.share({
+      title: 'WebShare API Demo',
+      url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+    } else {
+        toggle_share_bspopup();
+    }
+});
