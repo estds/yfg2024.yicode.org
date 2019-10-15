e0073e5d79dfaf98138701c072555d5b68d567e5
estds <56233807+estds@users.noreply.github.com>
2019-10-15 07:34:15 -0700

    Update estds.js

--- js/estds.js
+++ js/estds.js
@@ -79,7 +79,7 @@ if (this.hash !== "") {
   }, 800, function(){
 
     // Add hash (#) to URL when done scrolling (default click behavior)
-    window.location.hash = hash;
+    //window.location.hash = hash;
   });
 } // End if
 });
