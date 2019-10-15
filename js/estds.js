6c97d49ffcaf4f8e592de5fa013819240b780e48
estds <56233807+estds@users.noreply.github.com>
2019-10-14 17:37:10 -0700

    Update estds.js

--- js/estds.js
+++ js/estds.js
@@ -57,3 +57,56 @@ shareButton.addEventListener('click', event => {
     })
     .catch(console.error);
 });
+
+
+// Scrolls smoothly to anchor
+$(document).ready(function(){
+// Add smooth scrolling to all .sjump links
+$(".sjump").on('click', function(event) {
+
+// Make sure this.hash has a value before overriding default behavior
+if (this.hash !== "") {
+  // Prevent default anchor click behavior
+  event.preventDefault();
+
+  // Store hash
+  var hash = this.hash;
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+  });
+} // End if
+});
+});
+
+//#to-top button appears after scrolling
+var fixed = false;
+$(document).scroll(function() {
+if ($(this).scrollTop() > 250) {
+    if (!fixed) {
+	fixed = true;
+	// $('#to-top').css({position:'fixed', display:'block'});
+	$('#to-top').show("slow", function() {
+	    $('#to-top').css({
+		position: 'fixed',
+		display: 'block'
+	    });
+	});
+    }
+} else {
+    if (fixed) {
+	fixed = false;
+	$('#to-top').hide("slow", function() {
+	    $('#to-top').css({
+		display: 'none'
+	    });
+	});
+    }
+}
+});
